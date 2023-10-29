478e359752cb278f64b2532c5e8ddcad1a104ba4
rb <rkrb@tyfonmail.se>
2023-10-29 17:14:52 +0100

    Initial commit

--- CountingElementsCodility/Program.cs
+++ CountingElementsCodility/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿CountingElements test = new CountingElements();
+
+int[] arr = {1,3,1,4,2,3,5,4 };
+var result = test.FrogRiverOne(5, arr);
+Console.WriteLine(result);
+
+class CountingElements
+{
+   public int FrogRiverOne(int X, int[] A)
+   {
+       HashSet<int> position = new HashSet<int>();
+       
+       for (int i = 0; i < A.Length; i++)
+       {      
+          position.Add(A[i]);
+          if (position.Count == X)
+          {
+              return i;
+          }
+
+       }
+       return -1;
+
+ 
+   }
+}
